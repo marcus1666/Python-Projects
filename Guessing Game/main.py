4a8ef65c789de9b5caf86aded2977a5065c43c5e
marcusBYUI <ojo18002@byui.edu>
2022-01-13 08:19:15 +0100

    updated guesing game

--- Guessing Game/main.py
+++ Guessing Game/main.py
@@ -9,30 +9,29 @@ def level(mode):
     return lives
 
 
-def is_correct(guess, number, lives):
-    lives_over = False
-    while not lives_over:
+def is_correct(number, lives):
+    """Takes the guess, the number and amount of lives left as parameters
+    Then compares to give appropriate retuens"""
+    
+    while lives > 0:
+        print(f"You have {lives} attempts remaining to guess the number")
+        guess = guess_number()
         
-        if guess > number:
-            print("Too high \nGuess again")
-            lives -= 1
-            print(f"You have {lives} attempts remaining to guess the number")
-            guess = guess_number()
-            if lives == 1:
-                lives_over = True
-                print("You ran out of lives.... Sorry... GameOver")
-        elif guess < number:
-            print("Too low \nGuess again")
-            lives -= 1
-            print(f"You have {lives} attempts remaining to guess the number")
-            guess = guess_number()
-            if lives == 1:
-                lives_over = True
-                print("You ran out of lives.... Sorry... GameOver")
-        else:
+        if guess == number:
             print("You guessed right, You won")
-            lives_over = True
-                    
+            return
+            
+        elif guess > number:
+            print("Too high")
+            lives -= 1
+            
+      
+        elif guess < number:
+            print("Too low")
+            lives -= 1
+            
+         
+    print(f"You ran out of lives.... Sorry... GameOver\nThe number was {number}")                
     
 def guess_number():
     return int(input("Make a guess: "))
@@ -41,12 +40,9 @@ def main():
     print("Welcome to the number Guessing Game! \nI'm thinking of a number between 1 and 100.")
     
     lives = level(input("Choose a difficulty. Type 'easy' or 'hard': "))
-    
-    print(f"You have {lives} attempts remaining to guess the number")
     comp_number = random.randint(1, 101)
             
-    is_correct(guess_number(), comp_number, lives)
-    
-    
+    is_correct(comp_number, lives)
+ 
     
 main()
